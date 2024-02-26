a8f07db7712eb77d874e50f7f2c11c991ba21cdd
clarkbar88 <tclark@herrerainc.com>
2024-02-25 20:05:04 -0800

    fixed issue of object IDs not matching jurisdiction fix
    plot fix for new scales package issues

--- ScreeningModel.R
+++ ScreeningModel.R
@@ -13,15 +13,15 @@ library(cowplot)
 # 
 st_address<-st_read(paste0("https://gisdata.kingcounty.gov/arcgis/rest/services/",
                            "OpenDataPortal/transportation__st_address_line/MapServer/108/query?outFields=*&where=1%3D1&f=geojson"))
-# 
-# jurisdictions<-st_read(paste0("https://gisdata.kingcounty.gov/arcgis/rest/",
-#                           "services/OpenDataPortal/admin___base/MapServer/",
-#                           "446/query?outFields=*&where=1%3D1&f=geojson"))
-# st_address_juris<-st_address %>%
-#   select(OBJECTID,FULLNAME) %>%
-#   st_join(jurisdictions %>% select(CITYNAME)) %>%
-#   mutate(Juris=ifelse(grepl('FWY|SR|RAMP',FULLNAME),'WSDOT',CITYNAME)) %>%
-#   filter(!grepl('.',row.names(.),fixed = T))
+# NOTE OBJECT IDs are different between st_address and the KC_RAODS
+jurisdictions<-st_read(paste0("https://gisdata.kingcounty.gov/arcgis/rest/",
+                          "services/OpenDataPortal/admin___base/MapServer/",
+                          "446/query?outFields=*&where=1%3D1&f=geojson"))
+st_address_juris<-st_address %>%
+  select(FRADDL:FULLNAME,Shape_Length) %>%
+  st_join(jurisdictions %>% select(CITYNAME)) %>%
+  mutate(JurisFROMJOIN=ifelse(grepl('FWY|SR|RAMP',FULLNAME),'WSDOT',CITYNAME)) %>%
+  filter(!grepl('.',row.names(.),fixed = T))
 
 
 #Issues, 
@@ -35,12 +35,11 @@ st_address<-st_read(paste0("https://gisdata.kingcounty.gov/arcgis/rest/services/
 #I90 west of mercer is 0 ADT?
 
 #Please provide the shapefile rather than an excel export
-#missing vehicle weigth
 #we should add somethign for bridges
 
 kc_roads_SM<-read_excel('inputs/KC_Street_Address_SummaryAttributes_20240223_v2.xlsx') %>%
   transmute(OBJECTID,
-            FULLNAME,
+            FRADDL,FRADDR,TOADDL,TOADDR,FULLNAME,
             RoadSegmentID,
             KC_FCC=factor(KC_FCC,levels=c('L','C','M','P','F'),
                        labels=c('Local','Collector','Minor','Primary','Freeway')),
@@ -68,44 +67,16 @@ kc_roads_SM<-read_excel('inputs/KC_Street_Address_SummaryAttributes_20240223_v2.
             PctConveyed=ConveyLen_FT/Shape_Leng*100,
             PctConveyed=ifelse(PctConveyed>100,100,PctConveyed),
             BusTraffic =ifelse(is.na(BusTraffic ),0,BusTraffic ),
+            Shape_Length=Shape_Leng,
              RoadMiles=Shape_Leng/5280
          ) %>%
-  filter(Juris %in% c('WSDOT','King County') ) %>%
-  right_join(st_address %>% select(OBJECTID),.)
-
-nrow(kc_roads_SM) #89309 (when not using filter)
-nrow(st_address) #89313
-#missing four roads...
-# st_address %>%
-#   filter(!(OBJECTID %in% kc_roads_SM$OBJECTID))
-#OBJECID 1 - looks like a fake road? -1 for segiment ID, no name
-#BOREN-36052
-#BOSTON-38101
-#22ND-17351
-#  filter(Juris=="King County"|Juris=='WSDOT') %>%
-  #no longer needed
-  # left_join(emme_links %>% st_drop_geometry()%>% select(X.tveh,X.dyhtrk,X.dymtrk),
-  #           multiple='first',
-  #           by=c('ADT_PSRC'='X.tveh')) %>%
-  #adjust to remove freeway numbers from load roads
-
-  # mutate(ADT_PSRC=ifelse(KC_FCC=='Local'&ADT_PSRC>1000,
-  #                        rnorm(length(which(KC_FCC=='Local'&ADT_PSRC>1000)),
-  #                              mean(ADT_PSRC[KC_FCC=='Local'&ADT_PSRC<500]),
-  #                             sd(ADT_PSRC[KC_FCC=='Local'&ADT_PSRC<500])),
-  #                        ADT_PSRC),
-  #        MediumVehicle=ifelse(KC_FCC=='Local'&X.dymtrk>50,
-  #                                  median(X.dymtrk[KC_FCC=='Local'&X.dymtrk<50]),
-  #                            X.dymtrk),
-  #        HeavyVehicle=ifelse(KC_FCC=='Local'&X.dyhtrk>50,
-  #                            median(X.dyhtrk[KC_FCC=='Local'&X.dyhtrk<50]),
-  #                            X.dyhtrk))
-#issue with Freeways having zero, let's articially up those to 150001
- # mutate(ADT_PSRC=ifelse(grepl('FWY|SR',FULLNAME)&ADT_PSRC<1000,runif(1,15001,30000),ADT_PSRC))
-#add lookup for heavy vehicles from emme link
- # left_join(emme_links %>% st_drop_geometry() %>% select(X.tveh,X.dyhtrk) %>% distinct(),
-  #          by=c('ADT_PSRC'='X.tveh'))
-
+  right_join(st_address_juris %>% select(FRADDL:FULLNAME,Shape_Length,JurisFROMJOIN),.) %>%
+  mutate(JurisFinal=ifelse(Juris=='WSDOT',Juris,JurisFROMJOIN)) %>%
+  filter(JurisFinal %in% c('WSDOT','King County') ) %>%
+  arrange(OBJECTID)
+  
+nrow(kc_roads_SM) #16766
+nrow(st_address) #89371
 
 kc_roads_SM %>%
   st_drop_geometry() %>%
@@ -114,22 +85,20 @@ kc_roads_SM %>%
             AveRoadLength=mean(RoadMiles),
             TotalRoadMiles=sum(RoadMiles),
             AveADT=mean(ADT_PSRC,na.rm=T),
-            VehicleRoadMiles=sum(RoadMiles*ADT_PSRC,na.rm=T),
+          #  VehicleRoadMiles=sum(RoadMiles*ADT_PSRC,na.rm=T),
             AveMediumVehicles=mean(MediumVehicle,na.rm=T),
-            HeavyMediumRoadMiles=sum(RoadMiles*MediumVehicle,na.rm=T),
+          #  HeavyMediumRoadMiles=sum(RoadMiles*MediumVehicle,na.rm=T),
             #HeavyVehicles=mean(FreightRank,na.rm=T),
             AveHeavyVehicles=mean(HeavyVehicle,na.rm=T),
-            HeavyVehicleRoadMiles=sum(RoadMiles*HeavyVehicle,na.rm=T),
-            RoadMilesConvey=sum(RoadMiles*Convey,na.rm=T),
-            PctConveyed=RoadMilesConvey/TotalRoadMiles,
-            nConvey=sum(Convey),
-            PctConvey_Count=nConvey/n,
+           # HeavyVehicleRoadMiles=sum(RoadMiles*HeavyVehicle,na.rm=T),
+         #   RoadMilesConvey=sum(RoadMiles*Convey,na.rm=T),
+            PctConveyed=mean(PctConveyed,na.rm=T),
             RdSkirtPctImp=mean(RdSkirtPctImp,na.rm=T)) %>%
   write.csv('outputs/kc_wsdot_roads_summar.csv',row.names = F)
 
 #length of each road segement
 kc_roads_SM %>%
-  ggplot(aes(RoadMiles,fill=KC_FCC))+
+  ggplot(aes(RoadMiles,after_stat(count),fill=KC_FCC))+
   geom_density(alpha=0.5)+
   theme_bw()+
   scale_x_log10(label=scales::number,breaks=c(0.001,.01,0.1,0.5,1,5,10),
@@ -141,7 +110,8 @@ plot_dist_ADT_count<-kc_roads_SM %>%
   geom_density(alpha=.5)+
   geom_density(alpha=.5)+
   scale_x_log10('Average Daily Traffic',
-                limits=c(10,200000),breaks=c(100,500,1000,5000,10000,50000,100000,200000),minor_breaks=NULL,
+                limits=c(10,200000),#breaks=c(100,500,1000,5000,10000,50000,100000,200000),
+                minor_breaks=NULL,
                 labels=scales::label_number(scale_cut = scales::cut_short_scale()))+
   theme_bw()#+
 #  geom_vline(xintercept=c(500,5000,15000))+
@@ -152,7 +122,7 @@ plot_ADT_FCC<-kc_roads_SM %>%
   geom_density(alpha=.5)+
   geom_density(alpha=.5)+
   scale_x_log10('Average Daily Traffic',
-                limits=c(10,200000),breaks=c(100,500,1000,5000,10000,50000,100000,200000),minor_breaks=NULL,
+                limits=c(10,200000),#breaks=c(100,500,1000,5000,10000,50000,100000,200000),minor_breaks=NULL,
                 labels=scales::label_number(scale_cut = scales::cut_short_scale()))+
   theme_bw()#+
  # geom_vline(xintercept=c(500,5000,15000))+
@@ -164,7 +134,7 @@ plot_ADT_FCC_no_FW_locals<-kc_roads_SM %>%
   ggplot(aes(ADT_PSRC,fill=KC_FCC))+
   geom_density(alpha=.5)+
   scale_x_log10('Average Daily Traffic',
-                limits=c(10,200000),breaks=c(100,500,1000,5000,10000,50000,100000,200000),minor_breaks=NULL,
+                limits=c(10,200000),#breaks=c(100,500,1000,5000,10000,50000,100000,200000),minor_breaks=NULL,
                 labels=scales::label_number(scale_cut = scales::cut_short_scale()))+
   theme_bw()#+
  # geom_vline(xintercept=c(500,5000,15000))+
@@ -174,7 +144,7 @@ kc_roads_SM %>%
   ggplot(aes(MediumVehicle))+
   geom_density(alpha=.5)+
   scale_x_log10('Medium Vehicle Count (per day)',
-                limits=c(1,50000),breaks=c(10,50,100,500,1000,5000,10000,50000,100000,200000),minor_breaks=NULL,
+                limits=c(1,50000),#breaks=c(10,50,100,500,1000,5000,10000,50000,100000,200000),minor_breaks=NULL,
                 labels=scales::label_number(scale_cut = scales::cut_short_scale()))+
   theme_bw() #+
 #geom_vline(xintercept = c(5,50,1000))+
@@ -184,7 +154,7 @@ plot_dist_med_freight_count<-kc_roads_SM %>%
   ggplot(aes(MediumVehicle,fill=KC_FCC))+
   geom_density(alpha=.5)+
   scale_x_log10('Medium Vehicle Count (per day)',
-                limits=c(1,50000),breaks=c(10,50,100,500,1000,5000,10000,50000,100000,200000),minor_breaks=NULL,
+                limits=c(1,50000),#breaks=c(10,50,100,500,1000,5000,10000,50000,100000,200000),minor_breaks=NULL,
                 labels=scales::label_number(scale_cut = scales::cut_short_scale()))+
   theme_bw() #+
 #geom_vline(xintercept = c(5,50,1000))+
@@ -195,7 +165,7 @@ kc_roads_SM %>%
   ggplot(aes(HeavyVehicle))+
   geom_density(alpha=.5)+
   scale_x_log10('Heavy Vehicle Count (per day)',
-                limits=c(1,50000),breaks=c(10,50,100,500,1000,5000,10000,50000,100000,200000),minor_breaks=NULL,
+                limits=c(1,50000),#breaks=c(10,50,100,500,1000,5000,10000,50000,100000,200000),minor_breaks=NULL,
                 labels=scales::label_number(scale_cut = scales::cut_short_scale()))+
   theme_bw() #+
   #geom_vline(xintercept = c(5,50,1000))+
@@ -205,7 +175,7 @@ plot_dist_freight_count<-kc_roads_SM %>%
   ggplot(aes(HeavyVehicle,fill=KC_FCC))+
   geom_density(alpha=.5)+
   scale_x_log10('Heavy Vehicle Count (per day)',
-                limits=c(1,50000),breaks=c(10,50,100,500,1000,5000,10000,50000,100000,200000),minor_breaks=NULL,
+                limits=c(1,50000),#breaks=c(10,50,100,500,1000,5000,10000,50000,100000,200000),minor_breaks=NULL,
                 labels=scales::label_number(scale_cut = scales::cut_short_scale()))+
   theme_bw() #+
   #geom_vline(xintercept = c(5,50,1000))+
@@ -271,12 +241,12 @@ plot_convey_FCC<-kc_roads_SM %>%
   xlab('Conveyance (%)')
 ggsave('plots/conveyance_by_FCC.png',plot_convey_FCC,scale=0.8,width=10,height=4.5)
 
-plot_convey_FCC<-kc_roads_SM %>%
-  ggplot(aes(ConveyType))+
-  geom_bar(stat='count')+
-  scale_y_log10()+
-  facet_wrap(~KC_FCC)+
-  theme_bw()
+# plot_convey_FCC<-kc_roads_SM %>%
+#   ggplot(aes(ConveyType))+
+#   geom_bar(stat='count')+
+#   scale_y_log10()+
+#   facet_wrap(~KC_FCC)+
+#   theme_bw()
 
 plot_convey_imp<-kc_roads_SM %>%
   ggplot(aes(RdSkirtPctImp,fill=ConveyType))+
@@ -292,7 +262,7 @@ library(leaflet)
 #alternative scoring could be done so that the bins aren't artificially lost
 #e.g., (log10(Traffic*x*log10(HvyVeh))+log10(Connectedness)
 kc_roads_score_alternative<-kc_roads_SM %>%
-  transmute(OBJECTID,Juris,FULLNAME,RoadSegmentID,KC_FCC,
+  transmute(OBJECTID,JurisFinal,FULLNAME,RoadSegmentID,KC_FCC,
             RoadMiles,
             TrafficIntensity=ifelse(is.na(ADT_PSRC),1,ADT_PSRC),
             HeavyVehicleCount=ifelse(is.na(HeavyVehicle),1,HeavyVehicle),
@@ -335,23 +305,34 @@ plot_jitter_Scores_antiLog<-kc_roads_score_alternative %>%
   xlab('Road Classification')
 ggsave('plots/plot_jitter_Scores_antiLog.png',plot_jitter_Scores_antiLog,scale=0.8,width=10,height=4.5)
 
-top10_scores<-arrange(kc_roads_score_alternative,desc(TotalScore)) %>% st_drop_geometry()%>%slice(1:20) %>%
+top10_scores<-arrange(kc_roads_score_alternative,desc(TotalScore)) %>% st_drop_geometry()%>%slice(1:10) %>%
   mutate(RoadMiles=round(RoadMiles,1),
          across(c(TrafficIntensity:MediumVehicleCount,RoadwaySkirtImperviousness,PctConveyed),round)) %>%
   select(-ImpScore,-ConveyScore,-OBJECTID) 
 top10_scores%>%
   write.csv('outputs/top10_roads.csv',row.names = F)
 
+top10_scores_KC<-kc_roads_score_alternative %>%
+  filter(JurisFinal=='King County') %>%
+  arrange(desc(TotalScore)) %>% 
+  st_drop_geometry()%>%
+  slice(1:10) %>%
+  mutate(RoadMiles=round(RoadMiles,1),
+         across(c(TrafficIntensity:MediumVehicleCount,RoadwaySkirtImperviousness,PctConveyed),round)) %>%
+  select(-ImpScore,-ConveyScore,-OBJECTID) 
+top10_scores_KC%>%
+  write.csv('outputs/top10_roads_kc.csv',row.names = F)
+
 kc_roads_score_alternative %>%
   st_write('outputs/kc_roads_scored.shp',append = F)
 zip('outputs/kc_roads_scored.zip',list.files('outputs','kc_roads_scored',full.names = T))
 
 pal_alt<-colorNumeric('RdYlBu',0:6,reverse=T)
-kc_roads_score_alternative %>%
-#  filter(TotalScore>=5) %>%
+kc_roads_score_alternative %>% 
+  filter(!st_is_empty(.)) %>%
   leaflet() %>%
   addProviderTiles('Esri.WorldImagery') %>%
-  addPolylines(color=~pal_alt(TotalScore),popup = ~paste(FULLNAME,RoadSegmentID,KC_FCC,Juris,
+  addPolylines(color=~pal_alt(TotalScore),popup = ~paste(FULLNAME,RoadSegmentID,KC_FCC,JurisFinal,
                                                      paste('Total Score:',TotalScore),
                                                      paste('ADT:',round(TrafficIntensity)),
                                                      paste('Hvy Veh:',round(HeavyVehicleCount)),
